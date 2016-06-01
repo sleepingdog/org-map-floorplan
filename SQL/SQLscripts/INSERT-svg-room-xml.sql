eefa5d7835609b59fe26f07e80402091ee028b6c
Tavis Reddick <developer@sleepingdog.org.uk>
2016-06-01 23:42:37 +0100

    SQL scripts to create/populate location tables
    One script creates and populates the Campus, Building and Floor related
    tables, and creates the empty Room table. The other populates the Room
    table from the cleaned SVG file.

--- SQL/SQLscripts/INSERT-svg-room-xml.sql
+++ SQL/SQLscripts/INSERT-svg-room-xml.sql
@@ -5,13 +5,21 @@ DECLARE @SVG AS XML;
 SET @SVG = (SELECT CONVERT(xml, BulkColumn, 2) FROM OPENROWSET(
    BULK '\\MYBOOKLIVEDUO\sleepingdog\Projects\Coding\OrgMapFloorplan\XML\SVG\OrgMapFloorplanKCOB0-clean.svg',
    SINGLE_BLOB) AS x);
-/* check that the variable now holds the SVG */
-SELECT @SVG AS svg;
-/* write an XQuery statement to select rooms */
-SELECT @SVG.query('declare namespace svg="http://www.w3.org/2000/svg";         
-    /svg:svg/svg:g[substring(@id, 1, 1) = "F"]/svg:g[substring(@id, 1, 7) = "PH-OB-F"]
-') AS svg
-;
-/* write a query to insert room data including primary key and SVG into Room table */
-
-
+/* write a query to insert room data including multi-column primary key and SVG into Room table */
+WITH XMLNAMESPACES(
+	'http://www.w3.org/2000/svg' AS svg,
+	DEFAULT 'http://www.w3.org/2000/svg'
+		)
+--Insert each Room
+INSERT INTO dbo.Room(RoomID, FloorID, BuildingID, CampusID, SVG)
+SELECT
+	RoomSVG.value('substring(@id, 10, 3)', 'VARCHAR(5)') AS RoomID,
+	RoomSVG.value('substring(@id, 7, 2)', 'VARCHAR(5)') AS FloorID,
+	RoomSVG.value('substring(@id, 4, 2)', 'VARCHAR(5)') AS BuildingID,
+	RoomSVG.value('substring(@id, 1, 2)', 'VARCHAR(5)') AS CampusID,
+	RoomTable.RoomSVG.query('.') AS SVG -- the XML source itself
+FROM @SVG.nodes('/svg:svg/svg:g[substring(@id, 1, 1) = "F"]/svg:g[substring(@id, 1, 7) = "PH-OB-F" and substring(@id, 10, 1) = "R"]') AS RoomTable(RoomSVG);
+GO
+SELECT *
+FROM dbo.Room;
+GO
