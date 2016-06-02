77863a6c4c57b46485ee8d3e6b7aa505a2876a99
Tavis Reddick <developer@sleepingdog.org.uk>
2016-06-02 14:08:35 +0100

    Update Floor table with cleaned SVG
    SQL script takes the XML nodes representing floors in the cleaned SVG
    document (minus the Room nodes which are stored in the Room table) and
    stores them in the appropriate rows of the Floor table.

---
+++ SQL/SQLscripts/UPDATE-svg-floor-xml.sql
@@ -0,0 +1,40 @@
+/* shred XML from SVG into floor table (adapted from room population script) */
+USE sleeping_kelpiecollege;
+/* declare a variable to hold the SVG to be loaded from file */
+DECLARE @SVG AS XML;
+SET @SVG = (SELECT CONVERT(xml, BulkColumn, 2) FROM OPENROWSET(
+   BULK '\\MYBOOKLIVEDUO\sleepingdog\Projects\Coding\OrgMapFloorplan\XML\SVG\OrgMapFloorplanKCOB0-clean.svg',
+   SINGLE_BLOB) AS x);
+/* Modify the SVG variable to remove all Rooms from the Floors, as Rooms are stored separately. */
+SET @SVG.modify('
+  declare namespace svg="http://www.w3.org/2000/svg";  
+  delete /svg:svg/svg:g[substring(@id, 1, 1) = "F"]/svg:g[substring(@id, 1, 7) = "PH-OB-F" and substring(@id, 10, 1) = "R"]
+')  
+/* Declare and set the variables for the chosen campus and building */
+DECLARE @campusId AS VARCHAR(5) = 'PH', @buildingId AS VARCHAR(5) = 'OB';
+/* write a query to insert room data including multi-column primary key and SVG into Room table */
+WITH XMLNAMESPACES(
+	'http://www.w3.org/2000/svg' AS svg,
+	DEFAULT 'http://www.w3.org/2000/svg'
+		)
+/*
+--Test with selection first
+SELECT
+	FloorSVG.value('@id', 'VARCHAR(5)') AS FloorID,
+	@buildingId AS BuildingID,
+	@campusId AS CampusID,
+	FloorTable.FloorSVG.query('.') AS SVG -- the XML source itself minus the Room SVG nodes
+FROM @SVG.nodes('/svg:svg/svg:g[substring(@id, 1, 1) = "F"]') AS FloorTable(FloorSVG);
+*/
+--Update each Floor
+UPDATE dbo.Floor
+	SET SVG = FloorTable.FloorSVG.query('.') -- the XML source itself minus the Room SVG nodes
+	FROM @SVG.nodes('/svg:svg/svg:g[substring(@id, 1, 1) = "F"]') AS FloorTable(FloorSVG)
+	WHERE CampusID = @campusId
+		AND BuildingID = @buildingId
+		AND FloorId = FloorSVG.value('@id', 'VARCHAR(5)')
+;
+GO
+SELECT *
+FROM dbo.Floor;
+GO
